5c395433a7abcc4ddf0dc6aae05921d99486e3d5
Basil Nagle <sharpshot101@hotmail.co.uk>
2021-11-24 15:41:25 +0000

    Overwrite of files
    Uploaded files for this project to another repo by mistake and didn't know how to undo it. Overwriting this one with changes made in the other

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './App.css';
+import Header from "./components/Header";
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+
+const App = () => {
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctors appointment",
+        day: "Feb 5th, 2:30pm",
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: "Meeting at school",
+        day: "Feb 6th, 2:30pm",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 5th, 2:30pm",
+        reminder: false,
+    }
+])
+
+//Add a task
+// const addtask = (task) => {
+//   console.log(task)
+// }
+
+//delete a task
+const deleteTask = (id) => {
+  //console.log("Deleted a task!", id) →test functionality
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//toggle Reminder field
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder:
+      !task.reminder } : task
+    )
+  )
+  //console.log("ID used in toggleReminder is", id)
+}
+
+return (
+    <div className="container">
+      <Header />
+      <AddTask/>
+      {/* if tasks.length is greater than o, then display. showing nothing otherwise */}
+      {tasks.length > 0 ? (<Tasks tasks= {tasks} onDelete= {deleteTask} onToggle={toggleReminder}/>) : ("No Tasks to Show")}
+    </div>
+  );
+}
+
+export default App;
